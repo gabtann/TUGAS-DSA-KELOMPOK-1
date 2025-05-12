c8a9e90319ce85901310df30dead4b4b2102f408
fauziahmstf <fauziahmstfs@gmail.com>
2025-05-12 20:11:15 +0800

    Create final_solution.c

---
+++ D121241035_Nurul Fauziah Mustafa/final_solution.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+char stack[MAX];
+int top = -1;
+
+void push(char letter) {
+    if (top < MAX - 1) {
+        top++;
+        stack[top] = letter;
+    }
+}
+
+char pop() {
+    if (top >= 0) {
+        char letter = stack[top];
+        top--;
+        return letter;
+    }
+    return '\0';
+}
+
+void reverse(char *text) {
+    int i = 0;
+    while (text[i] != '\0') {
+        push(text[i]);
+        i++;
+    }
+
+    i = 0;
+    while (top >= 0) {
+        text[i] = pop();
+        i++;
+    }
+}
+
+int main() {
+    char kata[MAX];
+    printf("Masukkan kata: ");
+    scanf("%s", kata);
+    reverse(kata);
+    printf("Hasil dibalik: %s\n", kata);
+    return 0;
+}
